459c8f33f5a8fba3a6b1c60a2da321b7a4797127
Rafael Mattia <rdmattia@inf.ufpel.edu.br>
2022-06-26 00:14:43 -0300

    att

---
+++ Trabalhos01/ex014/readme.md
@@ -0,0 +1,4 @@
+# Trabalho 01
+Criar uma página web capaz de detectar e inserir a hora atual juntamente com uma imagem que represente o horário, escrevendo uma mensagem de bom dia/boa tarde/boa noite.
+
+
--- Trabalhos01/ex014/script.js
+++ Trabalhos01/ex014/script.js
@@ -3,18 +3,22 @@ function carregar() {
   var img = document.getElementById('imagem')
   var data = new Date()
   var hora = data.getHours()
+  var minutos = data.getMinutes()
   //var hora = 15 //linha para teste
-  msg.innerText = `Agora são ${hora} horas.`
+  msg.innerText = `Agora são ${hora} horas e ${minutos} minutos.`
   if (hora >= 0 && hora < 12){
     //BOM DIA
+    msg.innerHTML += `<p>Bom dia!</p>`
     img.src = 'imagens/manha.png'
     document.body.style.background = '#F1B56C'
   }else if(hora >=12 && hora <= 18){
     //BOA TARDE
+    msg.innerHTML += `<p>Boa tarde!</p>`
     img.src = 'imagens/tarde.png'
     document.body.style.background = '#91AAC0'
   }else{
     //BOA NOITE
+    msg.innerHTML += `<p>Boa noite!</p>`
     img.src = 'imagens/noite.png'
     document.body.style.background = '#372E4D'
   }
